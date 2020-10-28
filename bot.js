a827d2734a725e6ea6cb1230ddaa83aaa3acb680
Zarlengo <christopher@zarlengo.net>
2020-10-28 14:10:20 -0700

    Added private message (was channel) for start/stop

--- bot.js
+++ bot.js
@@ -1,25 +1,34 @@
+// Add discord dependency
 const Discord = require('discord.js');
-const auth = require('./auth.json');
-// Configure logger settings
-// Initialize Discord Bot
 
+// File containing secret keys (.sample is the file with the keys removed)
+const auth = require('./auth.json');
+
+// Initialize Discord Bot
 const bot = new Discord.Client();
 bot.login(auth.token);
 
+// Default poll settings
 const defaultGame = "Factorio";
 const defaultTime = "7:30 PM";
 
+// Variables
 let game = defaultGame;
 let gameTime = defaultTime;
-
-let reaction = false;
 let timerInterval = [];
 let running = false;
+const dayToMs = 24 * 60 * 60 * 1000; // Hours * Minutes * Seconds * Milliseconds
+let timeInterval = 7 * dayToMs;
 
-let timeInterval = 7 * 24 * 60 * 60 * 1000 // Days * Hours * Minutes * Seconds * Milliseconds
-let dateTime = Date.now();
+// Function to get the number of milliseconds between 'start' and sunday @ noon
+const getSunday = () => {
+    let currentDateTime = Date.now();
+    const today = new Date();
+    const sunday = today - today.getHours() * 60 * 60 * 1000 - today.getMinutes() * 60 * 1000 - today.getSeconds() * 1000 - today.getMilliseconds() + 13.59 * 60 * 60 * 1000;// + (7 - today.getDay()) * dayToMs;
+    return sunday - currentDateTime;
+}
 
-
+// Formatted help message for the bot
 const helpMessage = `gamePoll help
     Shows available commands
 
@@ -36,77 +45,126 @@ gamePoll status
     Shows the current state of the bot
 `
 
+// Console output when bot is first loaded
 bot.once('ready', () => {
     console.log(`Connected. Logged in as: ${ bot.user.username } (${ bot.user.id })`);
 });
 
+// Bot reaction to messages
 bot.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.content.substring(0, 1) == '!') {
+        // Gets the first word after ! to check if it is valid
         let args = message.content.substring(1).split(' ');
         let cmd = args[0].toLowerCase();
         args = args.splice(1);
         switch(cmd) {
+            // Cases !gp and !gamePoll (not case sensitive) perform bot actions
             case 'gp':
             case 'gamepoll':
+                // If there was an argument added to the command
                 if (args.length > 0) {
+                    // Gets any tertiary arguments (i.e. game name)
                     let cmdType = args[0].toLowerCase();
                     let cmdOptions = args.splice(1).join(" ");
+
+                    // Chooses the option based on the argument
                     switch(cmdType) {
+                        // Help screen
                         case 'help':
                             messageContent = helpMessage;                            
                             break;
+
+                        // Change game name
                         case 'game':
                             game = cmdOptions;
-                            running = false;
-                            for (let i = 0; i < timerInterval.length; i++) {
-                                clearTimeout(timerInterval[i]);
+
+                            // If the service is currently running, stops future messages and stops service
+                            if (running) {
+                                for (let i = 0; i < timerInterval.length; i++) {
+                                    clearTimeout(timerInterval[i]);
+                                }
+                                running = false;
                             }
                             messageContent = `Game changed to ${ game }. Service has been stopped`;
                             break;
+
+                        // Start weekly auto polling
                         case 'start':
-                            running = true;
-                            for (let i = 1; i <= 5; i ++) {
-                                timerInterval[i - 1] = setTimeout(() => {
-                                    sendMessage(message, pollMessage(game, gameTime), true);
-                                }, 3000 * i);
+                            // If the service is currently running, stops future messages
+                            if (running) {
+                                for (let i = 0; i < timerInterval.length; i++) {
+                                    clearTimeout(timerInterval[i]);
+                                }                                
                             }
+                            // Changes the running Boolean to true
+                            running = true;
+
+                            // Gets the milliseconds between now and sunday @ noon
+                            const timeToSunday = getSunday();
+
+                            // Performs 3 loops of the auto polling (setTimeout is limited to 25 days)
+                            for (let i = 0; i < 3; i ++) {
+                                timerInterval[i - 1] = setTimeout(() => {
+                                    sendChannelMessage(pollMessage(game, gameTime));
+                                }, timeInterval * i + timeToSunday);
+                            }
+
+                            // Sends a message to the author when the service has finished so they can start a new cycle
+                            timerInterval[3] = setTimeout(() => {
+                                sendAuthorMessage(message, "Service has completed. Enter '!gamePoll start' for the next cycle.");
+                                running = false;
+                            }, timeInterval * 2 + timeToSunday);
+
+                            // Message to the author that the service has been started successfully
                             messageContent = `Service has been started: ${ game} @ ${ gameTime }`;
                             break;
+
+                        // Stop auto polling
                         case 'stop':
+                            // Stops any future messages
                             running = false;
                             for (let i = 0; i < timerInterval.length; i++) {
                                 clearTimeout(timerInterval[i]);
                             }
+
+                            // Sends an update to the user
                             messageContent = "Service has been stopped";
                             break;
+
+                        // Gets the current state of the bot
                         case 'status':
-                            reaction = false;
                             if (running) {
                                 messageContent = `Service is running: ${ game} @ ${ gameTime }`;
                             } else {
                                 messageContent = `Service is not running: ${ game} @ ${ gameTime }`;
                             }
                             break;
+
+                        // If the argument is not on the list
                         default:
                             messageContent = `Unrecognized command: ${ args }`;
                     }
+
+                // Defaults to help message if no argument is included
                 } else {                    
                     messageContent = helpMessage;
                 }
                 break;
 
+            // Help message
             case 'help':
             default:
                 messageContent = helpMessage;
                 break;
         }
-        sendMessage(message, messageContent, reaction);
-        reaction = false;
+        // Function to send a direct reply back to the person, avoids channel talk
+        sendAuthorMessage(message, messageContent);
     }
 });
 
+// Function to get the poll message default format
 const pollMessage = (gameName = "Factorio", gameTime = "7:30") => {
     return `${ gameName} @ ${ gameTime }
   :regional_indicator_t: = Tuesday
@@ -114,13 +172,17 @@ const pollMessage = (gameName = "Factorio", gameTime = "7:30") => {
   :regional_indicator_f: = Friday`;
 }
 
-const sendMessage = (message, messageContent, reaction) => {
-    message.channel.send(messageContent)
+// Function to send a private message to the author
+const sendAuthorMessage = (message, messageContent) => {
+    message.author.send(messageContent);
+}
+
+// Function to send a message to the whole channel
+const sendChannelMessage = (messageContent) => {
+    bot.channels.cache.get(auth.channelID).send(messageContent)
         .then(response => {
-            if (reaction) {
-                response.react("🇹");
-                response.react("🇷");
-                response.react("🇫");
-            }
+            response.react("🇹");
+            response.react("🇷");
+            response.react("🇫");
         });
 }
